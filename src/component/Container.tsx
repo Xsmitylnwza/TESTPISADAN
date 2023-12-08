78cb11ad48c8957693b85127ac7f4de83f05a911
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2023-12-08 23:25:05 +0700

    ha

--- src/component/Container.tsx
+++ src/component/Container.tsx
@@ -1,4 +1,8 @@
-  import krugkein from "../assets/button เครื่องเขียน 274 x 330px.png";
+  import เครื่องเขียน from "../assets/button เครื่องเขียน 274 x 330px.png";
+  import เครื่องแต่งกาย from "../assets/button เครื่องแต่งกาย274 x 330px.png";
+  import หนังสือ from "../assets/button หนังสือ 274 x 330.png";
+  import อุปกรณ์กีฬา from "../assets/button อุปกรณ์กีฬา 274 x 330px.png";
+
   import ShopSectionBox01 from './ShopSectionBox'
   import ShopSectionBox02 from './ShopSectionBox02'
 
@@ -7,10 +11,10 @@
       <>
         <div className='bg-primary3 w-full'>
           <div className='flex justify-between p-menu-bar'>
-            <a href=""><img src={krugkein} width="274" height="330" alt="menu-เครื่องเขียน" /></a>
-            <a href=""><img src="/assets/button เครื่องแต่งกาย274 x 330px.png" width="274" height="330" alt="menu-แต่งกาย" /></a>
-            <a href=""><img src="/assets/button หนังสือ 274 x 330.png" width="274" height="330" alt="menu-หนังสือ" /></a>
-            <a href=""><img src="/assets/button อุปกรณ์กีฬา 274 x 330px.png" width="274" height="330" alt="menu-กีฬา" /></a>
+            <a href=""><img src={เครื่องเขียน} width="274" height="330" alt="menu-เครื่องเขียน" /></a>
+            <a href=""><img src={เครื่องแต่งกาย} width="274" height="330" alt="menu-แต่งกาย" /></a>
+            <a href=""><img src={หนังสือ} width="274" height="330" alt="menu-หนังสือ" /></a>
+            <a href=""><img src={อุปกรณ์กีฬา} width="274" height="330" alt="menu-กีฬา" /></a>
           </div>
           <div className='flex justify-center mt-shopsection'>
             <ShopSectionBox01 />
